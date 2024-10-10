84c7dc8007208d4bc088379124c50aa68164bed9
Siddharth Boorgu <siddharth.boorgu@xdp.co.in>
2024-10-10 16:12:59 +0530

    partyzFrontendApplication

--- config/useLocalConfig.tsx
+++ config/useLocalConfig.tsx
@@ -1,16 +1,16 @@
-export const GetFunctionHalls = "http://192.168.0.4:4000/api/getFunctionHalls"
-export const GetBookingDateDetails = "http://192.168.0.4:4000/api/getBookingDateDetails"
-export const SupervisorIdPostingBackend = "http://192.168.0.4:4000/api/postSupervisorUserId"
-export const getFunctionHallsBySupervisorUserId = "http://192.168.0.4:4000/api/getFunctionHallsBySupervisorUserId"
-export const PostUserDetails =  "http://192.168.0.4:4000/api/postUserDetails"
-export const PostBookingDatesDetails =  "http://192.168.0.4:4000/api/postUserDetails"
-export const PostUserPhoneNumber = "http://192.168.0.4:4000/api/userPhoneNumber"
-export const getAllUserBookings =  "http://192.168.0.4:4000/api/getAllUserBookings"
-export const postBookingDate =  "http://192.168.0.4:4000/api/postBookingDate"
+export const GetFunctionHalls = "http://192.168.0.2:4000/api/getFunctionHalls"
+export const GetBookingDateDetails = "http://192.168.0.2:4000/api/getBookingDateDetails"
+export const SupervisorIdPostingBackend = "http://192.168.0.2:4000/api/postSupervisorUserId"
+export const getFunctionHallsBySupervisorUserId = "http://192.168.0.2:4000/api/getFunctionHallsBySupervisorUserId"
+export const PostUserDetails =  "http://192.168.0.2:4000/api/postUserDetails"
+export const PostBookingDatesDetails =  "http://192.168.0.2:4000/api/postUserDetails"
+export const PostUserPhoneNumber = "http://192.168.0.2:4000/api/userPhoneNumber"
+export const getAllUserBookings =  "http://192.168.0.2:4000/api/getAllUserBookings"
+export const postBookingDate =  "http://192.168.0.2:4000/api/postBookingDate"
 
 
 // Define the base URL
-export const BASE_URL = "http://192.168.0.4:4000/api/updateRequestById/";
+export const BASE_URL = "http://192.168.0.2:4000/api/updateRequestById/";
 
 // Create a function to generate the full URL
 export const getUpdateBookingRequestUrl = (itemId) => `${BASE_URL}${itemId}`;
